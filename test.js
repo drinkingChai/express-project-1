5a186b200c69e97f7582c5598c2bfb76e4f4019a
Wasif Zaman <teatocode@gmail.com>
2016-10-21 15:50:36 -0400

    Add validations for new citiy

--- test.js
+++ test.js
@@ -66,6 +66,14 @@ describe('Creating new cities', function() {
       .send('name=Springfield&description=where+the+simpsons+live')
       .expect(/springfield/i, done);
   });
+
+  it('Validates city name and description', function(done) {
+      request(app)
+        .post('/cities')
+        .send('name=&description=')
+        .expect(404, done);
+  });
+
 });
 
 describe('Deleting cities', function() {
